2101d19dc233321776510630b2bbb08f70b1b57a
awakenedlilac <90451983+awakenedlilac@users.noreply.github.com>
2022-10-19 09:11:57 +0300

    lint

--- tasks_gen.py
+++ tasks_gen.py
@@ -127,7 +127,7 @@ class Storage:
 
         """saves original texts and tasks"""
 
-        print('smthh')
+        print('smthhh')
         doc_orig = Document()
         style = doc_orig.styles['Normal']
         style.font.name = 'Times New Roman'
