4b8a109cd21eddad75d18ca42b6900703df60a36
awakenedlilac <90451983+awakenedlilac@users.noreply.github.com>
2022-10-24 15:00:40 +0300

    lint

--- tasks_gen.py
+++ tasks_gen.py
@@ -2,7 +2,7 @@
 import re
 import random
 import pymorphy2
-from docx import Document
+import docx
 
 with open('text', 'r', encoding='utf-8') as f:
     texts = f.read()
@@ -131,8 +131,8 @@ class Storage:
     def __init__(self, saved_task):
         self.saved_task = saved_task
         self.original_texts = self.saved_task.getting_texts()
-        self.doc_orig = Document()
-        self.doc = Document()
+        self.doc_orig = docx.Document()
+        self.doc = docx.Document()
         self.save_original_texts()
         self.save_task_1()
         self.save_task_2()
