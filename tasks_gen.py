12245ca6e8e6d8e3dd07673efa928ea5afb517b7
awakenedlilac <90451983+awakenedlilac@users.noreply.github.com>
2022-10-24 14:14:48 +0300

    lint

--- tasks_gen.py
+++ tasks_gen.py
@@ -131,8 +131,8 @@ class Storage:
     def __init__(self, saved_task):
         self.saved_task = saved_task
         self.original_texts = self.saved_task.getting_texts()
-        self.doc_orig = docx.Document()
-        self.doc = docx.Document()
+        self.doc_orig = docx.Document(docx=None)
+        self.doc = docx.Document(docx=None)
         self.save_original_texts()
         self.save_task_1()
         self.save_task_2()
